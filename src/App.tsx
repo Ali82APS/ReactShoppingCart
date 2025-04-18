cc8442e2b5f8be80091c9f77f5c82e1fce1e85ed
Ali82Aps <a.82aps@gmail.com>
2025-04-19 02:10:24 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home.tsx";
+import Store from "./pages/store/Store.tsx";
+import Layout from "./components/layout/Layout.tsx";
+
+function App() {
+  return (
+    <>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/store/Store.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Store() {
+  return (
+    <>
+      <h1 className="text-right mt-5">جدیدترین محصولات</h1>
+
+      <div></div>
+    </>
+  );
+}
+
+export default Store;
